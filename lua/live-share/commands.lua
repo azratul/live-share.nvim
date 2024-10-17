c7dcf80dbbf6beed50419ecbf9310bfc499bdd1b
xiaojianzheng <1272209235@qq.com>
2024-10-17 16:01:11 +0800

    Style: format code

--- lua/live-share/commands.lua
+++ lua/live-share/commands.lua
@@ -1,18 +1,18 @@
 local M = {}
 
 function M.setup(config)
-    M.config = config
+  M.config = config
 end
 
 function M.start_live_share(port)
-    port = port or M.config.port_internal
-    require('live-share.instant').start(port)
-    require('live-share.tunnel').start(port)
+  port = port or M.config.port_internal
+  require("live-share.instant").start(port)
+  require("live-share.tunnel").start(port)
 end
 
 function M.join_live_share(url, port)
-    port = port or M.config.port
-    require('live-share.instant').join(url, port)
+  port = port or M.config.port
+  require("live-share.instant").join(url, port)
 end
 
 return M
--- lua/live-share/init.lua
+++ lua/live-share/init.lua
@@ -1,20 +1,20 @@
 local M = {}
 
 function M.setup(config)
-    config = config or {}
-    M.config = {
-        port = config.port or 80,
-        port_internal = config.port_internal or 9876,
-        max_attempts = config.max_attempts or 40,
-        service_url = config.service_url or os.getenv("tmp") .. "/service.url",
-        service = config.service or "nokey@localhost.run",
-        ip_local = config.ip_local or "127.0.0.1",
-        ssh_pid = nil
-    }
+  config = config or {}
+  M.config = {
+    port = config.port or 80,
+    port_internal = config.port_internal or 9876,
+    max_attempts = config.max_attempts or 40,
+    service_url = config.service_url or os.getenv("tmp") .. "/service.url",
+    service = config.service or "nokey@localhost.run",
+    ip_local = config.ip_local or "127.0.0.1",
+    ssh_pid = nil,
+  }
 
-    require('live-share.instant').setup(M.config)
-    require('live-share.tunnel').setup(M.config)
-    require('live-share.commands').setup(M.config)
+  require("live-share.instant").setup(M.config)
+  require("live-share.tunnel").setup(M.config)
+  require("live-share.commands").setup(M.config)
 end
 
 return M
