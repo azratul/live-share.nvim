deb930dee260c5b08f325d66eb4f3dd4bcd0bc19
xiaojianzheng <1272209235@qq.com>
2024-10-17 15:53:09 +0800

    Feat: service.url is compatible with Windows paths

--- lua/live-share/init.lua
+++ lua/live-share/init.lua
@@ -6,7 +6,7 @@ function M.setup(config)
         port = config.port or 80,
         port_internal = config.port_internal or 9876,
         max_attempts = config.max_attempts or 40,
-        service_url = config.service_url or "/tmp/service.url",
+        service_url = config.service_url or os.getenv("tmp") .. "/service.url",
         service = config.service or "nokey@localhost.run",
         ip_local = config.ip_local or "127.0.0.1",
         ssh_pid = nil
