9d904b7cdcda493ead710143a919f82bdebc3118
xiaojianzheng <1272209235@qq.com>
2024-10-18 09:28:14 +0800

    Fix: Compatible with multiple systems

--- lua/live-share/init.lua
+++ lua/live-share/init.lua
@@ -1,12 +1,24 @@
 local M = {}
 
+local get_default_service_url = function()
+  local temp_dir
+
+  if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    temp_dir = os.getenv("TEMP")
+  else
+    temp_dir = os.getenv("TMPDIR") or "/tmp"
+  end
+
+  return temp_dir .. "/service.url"
+end
+
 function M.setup(config)
   config = config or {}
   M.config = {
     port = config.port or 80,
     port_internal = config.port_internal or 9876,
     max_attempts = config.max_attempts or 40,
-    service_url = config.service_url or os.getenv("tmp") .. "/service.url",
+    service_url = config.service_url or get_default_service_url(),
     service = config.service or "nokey@localhost.run",
     ip_local = config.ip_local or "127.0.0.1",
     ssh_pid = nil,
